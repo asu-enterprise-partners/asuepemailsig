f5a9af5ab2b0647ce6b2a621a41cd780d2ae97f5
Cecilia Nguyen <clnguye8@asu.edu>
2019-04-26 13:14:31 -0700

    Instruction Preview Changes
    Changed the email signature thumbnail by adding a comma and added the accordian functionality

--- sigfiles/custom.js
+++ sigfiles/custom.js
@@ -287,6 +287,48 @@ function destroyElm2(elm) {
     $('#addressPO').append(val);
    }
 
+//additional functionality
+   var headers = $('#accordion .accordion-header');
+   var contentAreas = $('#accordion .ui-accordion-content ').hide();
+   var expandLink = $('.accordion-expand-all');
+
+   // add the accordion functionality
+   headers.click(function() {
+       var panel = $(this).next();
+       var isOpen = panel.is(':visible');
+
+       // open or close as necessary
+       panel[isOpen? 'slideUp': 'slideDown']()
+           // trigger the correct custom event
+           trigger(isOpen? 'hide': 'show');
+
+       // stop the link from causing a pagescroll
+       return false;
+   });
+
+   // when panels open or close, check to see if they're all open
+   contentAreas.on({
+       // whenever we open a panel, check to see if they're all open
+       // if all open, swap the button to collapser
+       show: function(){
+           var isAllOpen = !contentAreas.is(':hidden');
+           if(isAllOpen){
+               expandLink.text('Collapse All')
+                   .data('isAllOpen', true);
+           }
+       },
+       // whenever we close a panel, check to see if they're all open
+       // if not all open, swap the button to expander
+       hide: function(){
+           var isAllOpen = !contentAreas.is(':hidden');
+           if(!isAllOpen){
+               expandLink.text('Expand all')
+               .data('isAllOpen', false);
+           }
+       }
+   });
+
+
    // if($('#addLine2').val() != '') {
    // var val = '<tr>' +
    //     '<td>'+
